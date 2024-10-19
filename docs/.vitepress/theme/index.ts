b0923fa04620c948a277a50375ce070d5e161840
wling <wlingzhenyu@163.com>
2024-10-20 04:08:00 +0800

    ✨ 明暗过度

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,11 +4,6 @@ import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
 
-import {
-  NolebaseEnhancedReadabilitiesMenu,
-  NolebaseEnhancedReadabilitiesScreenMenu,
-} from "@nolebase/vitepress-plugin-enhanced-readabilities/client";
-
 import "@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css";
 
 import type { Options } from "@nolebase/vitepress-plugin-enhanced-readabilities/client";
@@ -18,26 +13,16 @@ import { NolebaseInlineLinkPreviewPlugin } from "@nolebase/vitepress-plugin-inli
 
 import "@nolebase/vitepress-plugin-inline-link-preview/client/style.css";
 
-import { NolebaseHighlightTargetedHeading } from "@nolebase/vitepress-plugin-highlight-targeted-heading/client";
-
 import "@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css";
 
 import vitepressNprogress from "vitepress-plugin-nprogress";
 import "vitepress-plugin-nprogress/lib/css/index.css";
 import "@nolebase/vitepress-plugin-enhanced-mark/client/style.css";
+import MyLayout from "./components/MyLayout.vue";
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // 为较宽的屏幕的导航栏添加阅读增强菜单
-      "nav-bar-content-after": () => h(NolebaseEnhancedReadabilitiesMenu),
-      // 为较窄的屏幕（通常是小于 iPad Mini）添加阅读增强菜单
-      "nav-screen-content-after": () =>
-        h(NolebaseEnhancedReadabilitiesScreenMenu),
-      "layout-top": () => h(NolebaseHighlightTargetedHeading),
-    });
-  },
+  Layout: MyLayout,
   enhanceApp: (ctx) => {
     vitepressNprogress(ctx);
 
